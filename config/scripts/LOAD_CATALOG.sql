1708f28a7f1bbf0e7513b7c2c2be55148502e359
TeOolong <byteoolong@gmail.com>
2023-05-07 21:00:20 -0500

    Database Fast App

---
+++ config/scripts/LOAD_CATALOG.sql
@@ -0,0 +1,28 @@
+INSERT INTO ANP (ANP_NAME, ANP_DESCRIPTION, ANP_PRICE, ANP_IMAGE)  
+VALUES 
+('Parque Nacional Gueppí-Sekime',
+'El Parque Nacional Gueppí-Sekime se ubica en la zona fronteriza limitando por el oeste y el norte con Ecuador, por el este con la Reserva Comunal Huimeki y por el sur con la Reserva Comunal Airo Pai. Está rodeado por los ríos Lagartococha, Gueppí, Peneya y en su interior discurre el río Anusilla.
+Los suelos generalmente son profundos, de baja fertilidad natural, ácidos y con baja saturación de bases, lo cual indica que su ámbito mayormente es de capacidad forestal y de protección. Además presenta paisajes de llanura de inundación, paisajes de colinas y terrazas altas, que albergan una amplia diversidad de flora silvestre.',
+20, 'https://www.gob.pe/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbDYyIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--ab8ffe5fa4b88add594713b9caab2d300916ea00/Parque%20Nacional%20Guepp%C3%AD%20Sekime.jpg'),
+('Reserva Nacional Dorsal de Nasca',
+'La Dorsal de Nasca es una cadena de montañas submarinas que forman parte de la cordillera Dorsal de Nasca, Salas y Gómez, cadenas de montes submarinos de origen volcánico que tiene más de 4000 km de extensión y cruza casi todo el Pacífico Sur. Tiene origen volcánico y se formó hace aproximadamente 30 millones de años. A diferencia de otras cordilleras submarinas del mundo, esta se encuentra a grandes profundidades. Su superficie es irregular, y presenta fosas, cañones, terrazas, cimas, cuevas y cráteres, generando ambientes únicos de importancia ecológica.',
+30.50, 'https://www.gob.pe/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBdEsyIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--bf021ee09468d59a87e1dc5a796f5f98bceb8ff4/Reserva%20Nacional%20Dorsal%20de%20Nasca%20-%20Eduardo%20Sorensen.jpg'),
+('Santuario Nacional Cordillera de Colán',
+'El Santuario Nacional Cordillera de Colán comprende una muestra representativa de los bosques montanos o yungas del norte del Perú, en sus tres pisos altitudinales: premontano, montano bajo y montano.
+Como resultado de una combinación de aspectos climáticos y geomorfológicos locales, gran parte de estos bosques corresponden al bioma de bosques de neblina o Ceja de Selva. Esta zona es de gran importancia por ser un lugar prioritario para la conservación y por tener una alta capacidad generadora y reguladora de agua de los bosques de neblina.',
+25, 'https://www.gob.pe/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBb2F3IiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--bbd0344c34d8f9a90fcb45c36f6a0b9ac36eee5f/SN%20Cordillera%20de%20Col%C3%A1n.jpg'),
+('Santuario Histórico de Machupicchu',
+'El Santuario Histórico de Machupicchu es un área natural protegida de renombre internacional, muy conocida por la presencia de impresionantes complejos arqueológicos Inca, sitios y monumentos arqueológicos de alto valor histórico-cultural.
+Sin embargo, el santuario presenta también un importante valor ambiental, con áreas boscosas, montañas escarpadas y picos nevados. Es una zona de interacción entre el dominio andino y el amazónico; y forma parte de una estrecha faja de transición entre ambos ecosistemas. Predomina el terreno de relieve abrupto y de laderas escarpadas.',
+20, 'https://portal.andina.pe/EDPfotografia/Thumbnail/2015/04/21/000290460W.jpg'),
+('Reserva Paisajística Subcuenca del Cotahuasi',
+'La Reserva Paisajística Subcuenca del Cotahuasi es el área natural protegida más extensa de la sierra peruana. Posee uno de los cañones más profundos y hermosos del planeta, el cual se extiende desde las faldas del nevado Solimana hasta la confluencia con el río Ocoña. En el sector de Ninacocha, el cañón supera los 3,500 m.s.n.m.
+Es una muestra representativa de los Andes Tropicales del sur del Perú que, por su localización en los Andes Meridionales y en las partes altas del río Ocoña, dispone de condiciones geográficas únicas que originan una diversidad biológica de importancia global en cada una de sus doce zonas de vida, las mismas que se distribuyen en un espacio que va desde los 950 m.s.n.m y se eleva por el cañón hasta nevados superiores a los 6,100 m.s.n.m.',
+27.50, 'https://www.gob.pe/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbUtwIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--99d487e0e0abe7f440760418c595ebd7cc3d514b/Reserva%20Paisaj%C3%ADstica%20Subcuenca%20del%20Cotahuasi.jpg'),
+('Refugio de Vida Silvestre Bosques Nublados de Udima',
+'Los bosques de neblina se presentan principalmente entre 2,000 y 3,000 metros de altitud, por lo que las temperaturas medias anuales en esta zona fluctúan aproximadamente entre 22°C a 500m, 18°C a 1,500m, 12°C a 2,500m y 8°C a 3,500m.
+La importancia de estos bosques no solo radica en su riqueza biológica y cultural, presente a lo largo de toda su gradiente altitudinal, sino que también juegan un papel primordial en el abastecimiento de agua limpia a todo el valle, que incluso tiene influencia sobre grandes ciudades como Chiclayo, cuyas aguas para consumo humano, agricultura e industria, se originan en estos bosques.',
+15, 'https://www.gob.pe/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBa2VwIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--7c45580531f1c499dc63327ab37b01ea63c123d6/RVS%20Bosques%20Nublados%20de%20Udima.jpg');
+
+
+
